9a59921f4f015f47ffcdd763a8087017e491c8c1
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-20 20:08:34 +0600

    if statement removed

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,14 +3,9 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
-        /*if(number > 999 || number < 100) {
-            System.out.println("Incorrect number format");
-            return;
-        }*/
         int numBeginToEnd = number / 100;
         int numCenter = (number / 10) % 10;
         int numEndToBegin = number % 10;
         System.out.println(numEndToBegin * 100 + numCenter * 10 + numBeginToEnd);
     }
-
 }
