d99806854ac1de04eaf392187cb44b3e2551bf77
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-20 20:04:34 +0600

    hope last changes

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,10 +3,10 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
-        if(number > 999 || number < 100) {
+        /*if(number > 999 || number < 100) {
             System.out.println("Incorrect number format");
             return;
-        }
+        }*/
         int numBeginToEnd = number / 100;
         int numCenter = (number / 10) % 10;
         int numEndToBegin = number % 10;
